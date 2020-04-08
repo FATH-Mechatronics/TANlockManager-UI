b921eafe725c4ded7cb856dbdcb934c7596b872e
cs8898 <cs8898@gmx.de>
2020-04-08 22:21:07 +0200

    abortable Plugin Conf

--- pages/settings/plugin/_plugin.vue
+++ pages/settings/plugin/_plugin.vue
@@ -1,83 +1,84 @@
 <template>
-  <v-layout xs12 sm8 md6>
-    <v-flex>
-      <v-card>
-        <v-card-title class="headline mt-2">{{plugin}} Config</v-card-title>
-        <v-card-text>
-          <v-textarea
-            v-model="pluginConfig"
-            auto-grow
-            :success="correctJSON(pluginConfig)"
-            :error="!correctJSON(pluginConfig)"
-          />
-        </v-card-text>
-        <v-card-actions>
-          <v-btn :disabled="!correctJSON(pluginConfig)" @click="submitConfig">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-      <v-card>
-        <v-card-title class="headline mt-2">{{plugin}} Help</v-card-title>
-        <v-card-text>
-          <v-textarea v-model="pluginHelp" auto-grow disabled/>
-        </v-card-text>
-      </v-card>
-    </v-flex>
-  </v-layout>
+    <v-layout xs12 sm8 md6>
+        <v-flex>
+            <v-card>
+                <v-card-title class="headline mt-2">{{plugin}} Config</v-card-title>
+                <v-card-text>
+                    <v-textarea
+                            v-model="pluginConfig"
+                            auto-grow
+                            :success="correctJSON(pluginConfig)"
+                            :error="!correctJSON(pluginConfig)"
+                    />
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn @click="abortConfig">Abort</v-btn>
+                    <v-spacer/>
+                    <v-btn :disabled="!correctJSON(pluginConfig)" @click="submitConfig">Save</v-btn>
+                </v-card-actions>
+            </v-card>
+            <v-card>
+                <v-card-title class="headline mt-2">{{plugin}} Help</v-card-title>
+                <v-card-text>
+                    <v-textarea v-model="pluginHelp" auto-grow disabled/>
+                </v-card-text>
+            </v-card>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script lang="ts">
-  import {Component, Vue} from "nuxt-property-decorator";
+    import {Component, Vue} from "nuxt-property-decorator";
 
-  @Component
-  class PluginSettingsPage extends Vue {
-    pluginConfig: any = "";
-    pluginHelp: any = "";
-    plugin: string = "";
+    @Component
+    class PluginSettingsPage extends Vue {
+        pluginConfig: any = "";
+        pluginHelp: any = "";
+        plugin: string = "";
 
-    mounted() {
-      this.plugin = this.$route.params.plugin;
+        mounted() {
+            this.plugin = this.$route.params.plugin;
 
-      this.$axios
-        .get(`/config/plugin/${this.plugin}`)
-        .then(data => {
-          this.pluginConfig = JSON.stringify(data.data, null, 2);
-          this.$axios
-            .get(`/config/plugin/${this.plugin}/help`)
-            .then(data => {
-              this.pluginHelp = data.data;
-              this.$forceUpdate();
-            })
-            .catch(err => console.error(err));
-        })
-        .catch(err => console.error(err));
-    }
-
-    correctJSON(json: string) {
-      try {
-        let out = JSON.parse(json);
-        if (out != undefined) {
-          return true;
-        } else {
-          return false;
+            this.$axios
+                .get(`/config/plugin/${this.plugin}`)
+                .then(data => {
+                    this.pluginConfig = JSON.stringify(data.data, null, 2);
+                    this.$axios
+                        .get(`/config/plugin/${this.plugin}/help`)
+                        .then(data => {
+                            this.pluginHelp = data.data;
+                            this.$forceUpdate();
+                        })
+                        .catch(err => console.error(err));
+                })
+                .catch(err => console.error(err));
         }
-      } catch (e) {
-        return false;
-      }
-      return true;
+
+        correctJSON(json: string) {
+            try {
+                let out = JSON.parse(json);
+                return out !== undefined;
+            } catch (e) {
+                return false;
+            }
+        }
+
+        submitConfig() {
+            console.log("submitConfig", JSON.parse(this.pluginConfig));
+            this.$axios
+                .post(`/config/plugin/${this.plugin}`, JSON.parse(this.pluginConfig))
+                .then(data => {
+                    this.$router.back();
+                })
+                .catch(err => {
+                    alert(err);
+                });
+        }
+
+        abortConfig() {
+            this.$router.back();
+        }
     }
 
-    submitConfig() {
-      console.log("submitConfig", JSON.parse(this.pluginConfig));
-      this.$axios
-        .post(`/config/plugin/${this.plugin}`, JSON.parse(this.pluginConfig))
-        .then(data => {
-          this.$router.back();
-        })
-        .catch(err => {
-          alert(err);
-        });
-    }
-  }
-
-  export default PluginSettingsPage;
+    export default PluginSettingsPage;
 </script>
